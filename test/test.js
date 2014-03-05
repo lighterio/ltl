963914f4f260fe7d87be63145a5b70b615b430f0
Kaushik Shankar <skaushik92@gmail.com>
2014-03-05 02:41:50 -0800

    Local variables do not pollute export space.

--- test/test.js
+++ test/test.js
@@ -4,13 +4,17 @@ describe('ltl', function(){
 	it('should be an object', function() {
 		ltl.should.be.an.Object;
 	})
-	describe('#compile()', function(){
+	describe('.compile', function(){
 		it('should be a function', function() {
 			ltl.compile.should.be.a.Function;
 		})
+	})
+	describe('.compile("")', function(){
 		it('should return a function', function() {
+			// Input
 			var template = "";
-			ltl.compile("").should.be.a.Function;
+
+			ltl.compile(template).should.be.a.Function;
 		})
 	})
 })
