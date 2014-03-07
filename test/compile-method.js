cb4097285a9126b8fc567ef554802d673cf01496
Sam Eubank <sam@piephp.com>
2014-03-07 07:29:44 -0800

    Added tests for whitespace and nesting

--- test/compile-method.js
+++ test/compile-method.js
@@ -3,18 +3,18 @@ var ltl = require('../index');
 describe('ltl', function(){
 	it('should be an object', function() {
 		ltl.should.be.an.Object;
-	})
+	});
 	describe('.compile', function(){
 		it('should be a function', function() {
 			ltl.compile.should.be.a.Function;
-		})
-	})
+		});
+	});
 	describe('.compile("")', function(){
 		it('should return a function', function() {
 			// Input
 			var template = "";
 
 			ltl.compile(template).should.be.a.Function;
-		})
-	})
-})
+		});
+	});
+});
---
+++ test/nesting.js
@@ -0,0 +1,29 @@
+var ltl = require('../index');
+var assert = require('assert');
+
+describe('Nesting', function () {
+	describe('Two children', function () {
+		it('should be adjacent', function () {
+			var result = ltl.compile('ul\n li\n li')();
+			assert.equal(result, '<ul><li></li><li></li></ul>');
+		});
+	});
+	describe('Single parents', function() {
+		it('should each have a child', function() {
+			var result = ltl.compile('ul\n li\nul\n li')();
+			assert.equal(result, '<ul><li></li></ul><ul><li></li></ul>');
+		});
+	});
+	describe('Unbalanced tree', function () {
+		it('should be unbalanced', function () {
+			var result = ltl.compile('ul\n li\nul')();
+			assert.equal(result, '<ul><li></li></ul><ul></ul>');
+		});
+	});
+	describe('Many levels', function () {
+		it('should be nested', function () {
+			var result = ltl.compile('b\n b\n  b\n   b\n')();
+			assert.equal(result, '<b><b><b><b></b></b></b></b>');
+		});
+	});
+});
