97c60ad2c8bc0326730efcbd8159a8949512a479
Sam Eubank <sameubank+zerious@gmail.com>
2014-03-09 03:45:53 -0700

    Refactored tag/id/class/attribute parsing to deal with delimiters

--- test/tags.js
+++ test/tags.js
@@ -18,8 +18,24 @@ describe('Tags', function () {
 		var result = ltl.compile('.b.c')();
 		assert.equal(result, '<div class="b c"></div>');
 	});
+	it('should assume <div> when using attributes with no tag', function () {
+		var result = ltl.compile('(style="color:#f00")')();
+		assert.equal(result, '<div style="color:#f00"></div>');
+	});
 	it('should work with a tag and ID', function () {
 		var result = ltl.compile('a#a')();
 		assert.equal(result, '<a id="a"></a>');
+	});
+	it('should work with a tag and class', function () {
+		var result = ltl.compile('i.icon')();
+		assert.equal(result, '<i class="icon"></i>');
+	});
+	it('should work with a tag, id and class', function () {
+		var result = ltl.compile('i#save.icon')();
+		assert.equal(result, '<i id="save" class="icon"></i>');
+	});
+	it('should work with attributes', function () {
+		var result = ltl.compile('a(href="/")')();
+		assert.equal(result, '<a href="/"></a>');
 	});
 });
