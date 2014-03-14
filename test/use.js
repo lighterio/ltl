b2dcfde36a9d62f55c87aa748677c8259aba3a27
Sam Eubank <sameubank+zerious@gmail.com>
2014-03-13 19:54:40 -0700

    Changed "key" to "name" for template names

--- test/use.js
+++ test/use.js
@@ -6,18 +6,18 @@ describe('Includes', function () {
 		assert.object(ltl.cache);
 	});
 	it('should put templates in the cache', function () {
-		ltl.compile('b #{text}', {key: 'bold'});
+		ltl.compile('b #{text}', {name: 'bold'});
 		assert.func(ltl.cache.bold);
 	});
 	it('should include templates', function () {
-		var temp = ltl.compile('p\n use common', {key: 'temp'});
-		ltl.compile('b #{text}', {key: 'common'});
+		var temp = ltl.compile('p\n use common', {name: 'temp'});
+		ltl.compile('b #{text}', {name: 'common'});
 		var result = ltl.cache.temp({text: 'Hi!'});
 		assert.equal(result, '<p><b>Hi!</b></p>');
 	});
 	it('should include templates', function () {
-		var temp = ltl.compile('use base\n set a\n  p A\n set b\n  p B', {key: 'temp'});
-		var base = ltl.compile('div\n get a\n get b', {key: 'base'});
+		var temp = ltl.compile('use base\n set a\n  p A\n set b\n  p B', {name: 'temp'});
+		var base = ltl.compile('div\n get a\n get b', {name: 'base'});
 		var result = ltl.cache.temp();
 		assert.equal(result, '<div><p>A</p><p>B</p></div>');
 	});
